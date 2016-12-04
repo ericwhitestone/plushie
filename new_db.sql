8adb63e0d0e2307dd119123e41d664cdea2c0def
Eric Whitestone <eric.whitestone@gmail.com>
2016-12-04 18:52:19 -0500

    Add default values

--- new_db.sql
+++ new_db.sql
@@ -1,8 +1,8 @@
 
 
 create table barcode(pkey INTEGER PRIMARY KEY asc, 
-	barcode_value TEXT UNIQUE NOT NULL, freeplays INTEGER,
+	barcode_value TEXT UNIQUE NOT NULL, freeplays INTEGER DEFAULT 0,
 	added_by_admin INTEGER DEFAULT 0);
 create table access_log(pkey INTEGER PRIMARY_KEY asc,
 	 barcode_fk INTEGER REFERENCES barcode(pkey),
-	 access_timestamp DEFAULT CURRENT_TIMESTAMP);
+	 access_timestamp DEFAULT(CURRENT_TIMESTAMP));
