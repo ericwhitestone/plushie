9aa8be653c8a4d6164d4358c61e737cc4a49ca2e
Eric Whitestone <eric.whitestone@gmail.com>
2016-12-26 14:08:34 -0500

    Script for adding a special barcode

---
+++ add_barcode.sh
@@ -0,0 +1,19 @@
+DB="plushie.db"
+
+usage()
+{
+	echo "$0 barcode #freeplays"
+}
+
+if [ ! -e $DB ]; then
+	echo "The file $DB does not exist. Run the create script first."
+	exit 2
+fi
+
+if [ $# -ne 2 ]; then
+	usage
+	exit 2
+fi
+
+sql="insert into barcode(barcode_value, freeplays) values('$1', '$2')";
+sqlite3 $DB "$sql"
