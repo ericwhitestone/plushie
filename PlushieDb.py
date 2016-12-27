d96c013d9b93d4c44d61331f3ef28cd202c41c67
Eric Whitestone <eric.whitestone@gmail.com>
2016-12-26 19:39:52 -0500

    Create db module, implement a get barcode record

---
+++ PlushieDb.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+
+class PlushieDb:
+	STMT_BARCODE_BY_VALUE = ("select pkey, barcode_value "
+	"freeplays, added_by_admin from barcode where barcode_value = ?")
+		
+	def __init__(self, dbfile):
+		self.dbfile = dbfile
+	
+	def retrieveBarcode(self, bc_value):
+		con = sqlite3.connect(self.dbfile)
+		cur = con.cursor()
+		print (PlushieDb.STMT_BARCODE_BY_VALUE)
+		result = cur.execute(PlushieDb.STMT_BARCODE_BY_VALUE, (bc_value))
+		print (result)
--- plushie_server.py
+++ plushie_server.py
@@ -5,12 +5,17 @@ from urlparse import urlparse
 from urlparse import parse_qs
 import os
 import sys
+from PlushieDb import PlushieDb
 
 HOST_NAME = '192.168.1.5'
 PORT_NUMBER = 8080
 DB_FILE = 'plushie.db'
+QS_PARAM_BARCODE = 'barcode'
+QS_PARAM_SCANNER_ID = 'scannerId'
 
 class PlushieHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	
+		
 	def do_GET(s):
 		parsed_url = urlparse(s.path)
 		if parsed_url.path == '/authorizedPlay':
@@ -19,25 +24,46 @@ class PlushieHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 			PlushieHandler.get_help(s, parsed_url)
 		else:
 			s.send_response(404)
-		
-		
-	def get_authorizedPlay(s, parsed_url):
-		
-		qs_args = parse_qs(parsed_url.query)
-		if 'barcode' not in qs_args:
+
+	def do_PUT(s):
+		parsed_url = urlparse(s.path)
+		if parsed_url.path == '/authorizedPlay':
+			PlushieHandler.put_authorizedPlay(s, parsed_url)
+		else:
+			s.send_response(404)
+
+	def parse_params(self, parsed_url):
+		''' return dict of the url params, null if invalid '''
+		qs_args = parse_qs(parsed_url.query)	
+		if QS_PARAM_BARCODE not in qs_args:
+			return None
+		if QS_PARAM_SCANNER_ID not in qs_args:
+			return None
+		return qs_args
+	
+	def put_authorizedPlay(self, parsed_url):
+		qs_args = PlushieHandler.parse_params(self, parsed_url)
+		if qs_args is None:
 			s.send_response(404)
 			return
-		if 'scannerId' not in qs_args:
+		
+	def get_authorizedPlay(self, parsed_url):
+		data_access = PlushieDb(DB_FILE)	
+		qs_args = PlushieHandler.parse_params(self, parsed_url)
+		if qs_args is None: 
 			s.send_response(404)
 			return
+		
 		barcode = qs_args['barcode']
 		scanner_id = qs_args['scannerId']
 		print ( 'received args: %s' % qs_args)
+		barcode_record = data_access.retrieveBarcode(
+		barcode)	
 		"""Respond to GET"""
-		s.send_response(200)
-		s.send_header("Content-type", "application/json")
-		s.end_headers()
-		s.wfile.write("{status:ok}")
+		self.send_response(200)
+		self.send_header("Content-type", "application/json")
+		self.end_headers()
+		self.wfile.write("{status:ok}")
 
 	def get_help(s, parsed_url):
 		
